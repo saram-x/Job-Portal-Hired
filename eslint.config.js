8d774d868f02e14f2060818482242fc62baf5321
saram-x <saramkhan9077@gmail.com>
2025-04-13 11:26:46 +0000

    Enhanced ESLint config: Complete React linting rules and settings

--- eslint.config.js
+++ eslint.config.js
@@ -4,3 +4,17 @@ import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 
+export default [
+  { ignores: ['dist'] },
+  {
+    files: ['**/*.{js,jsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        ecmaFeatures: { jsx: true },
+        sourceType: 'module',
+      },
+    },
+    
