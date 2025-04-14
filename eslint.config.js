a0d5696ab7308f1ef0440f3df50dabb1d196c674
saram-x <saramkhan9077@gmail.com>
2025-04-14 05:33:25 +0000

    Complete ESLint setup: Full React 18 linting with hooks and refresh

--- eslint.config.js
+++ eslint.config.js
@@ -17,4 +17,15 @@ export default [
         sourceType: 'module',
       },
     },
-    
+    settings: { react: { version: '18.3' } },
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...react.configs.recommended.rules,
+      ...react.configs['jsx-runtime'].rules,
+      ...reactHooks.configs.recommended.rules,
+     
