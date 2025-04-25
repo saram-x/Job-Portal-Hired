8940eeedd9b8228e1a1f6a866626dfc0f7f3ee10
saram-x <saramkhan9077@gmail.com>
2025-04-25 18:58:32 +0000

    Added tailwind.config.js: Tailwind CSS configuration

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+// Tailwind CSS configuration with shadcn/ui theme integration
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	darkMode: ["class"],
+	content: [
+	  "./pages/**/*.{js,jsx}",
+	  "./components/**/*.{js,jsx}",
+	  "./app/**/*.{js,jsx}",
+	  "./src/**/*.{js,jsx}",
+	
