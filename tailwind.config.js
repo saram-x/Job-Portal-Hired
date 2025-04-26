a3fc0935d57b650e1aea0d441cd4cffc06f05999
saram-x <saramkhan9077@gmail.com>
2025-04-26 14:19:11 +0000

    Update tailwind.config.js: Tweaked Tailwind CSS settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,4 +7,13 @@ module.exports = {
 	  "./components/**/*.{js,jsx}",
 	  "./app/**/*.{js,jsx}",
 	  "./src/**/*.{js,jsx}",
-	
+	],
+	prefix: "",
+	theme: {
+    	container: {
+    		center: true,
+    		padding: '2rem',
+    		screens: {
+    			'2xl': '1400px'
+    		}
+    	},
