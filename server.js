7439006a57699d417d9494c81fcdbccb1ce09267
saram-x <saramkhan9077@gmail.com>
2025-05-11 09:17:54 +0000

    Update server.js Load environment variables

--- server.js
+++ server.js
@@ -4,3 +4,9 @@ import fetch from "node-fetch";
 import cors from "cors";
 import { createClient } from "@supabase/supabase-js";
 
+// Load environment variables
+dotenv.config({ debug: false, quiet: true });
+
+const app = express();
+const port = process.env.PORT || 3001;
+
