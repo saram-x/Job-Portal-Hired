59315de56170e317e4966ff4d091b6583a1f257f
saram-x <saramkhan9077@gmail.com>
2025-05-28 18:39:34 +0000

    Add main App component for job portal UI

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+// Import layout and route protection components
+import AppLayout from "./layouts/app-layout";
+import ProtectedRoute from "./components/protected-route";
+import { ThemeProvider } from "./components/theme-provider";
+import { Toaster } from "./components/ui/toaster";
+
