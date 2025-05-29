b74b955d93549073685fdba282aa33acd10f88b1
saram-x <saramkhan9077@gmail.com>
2025-05-29 11:43:13 +0000

    Refactor App.jsx: Improve job portal UI and component structure

--- src/App.jsx
+++ src/App.jsx
@@ -6,3 +6,15 @@ import ProtectedRoute from "./components/protected-route";
 import { ThemeProvider } from "./components/theme-provider";
 import { Toaster } from "./components/ui/toaster";
 
+// Import all page components
+import AdminPage from "./pages/AdminPage";
+import LandingPage from "./pages/landing";
+import Onboarding from "./pages/onboarding";
+import PostJob from "./pages/post-job";
+import JobListing from "./pages/jobListing";
+import MyJobs from "./pages/my-jobs";
+import SavedJobs from "./pages/saved-jobs";
+import JobPage from "./pages/job";
+
+import "./App.css";
+
