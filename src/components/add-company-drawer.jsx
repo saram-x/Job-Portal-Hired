eeb817f9fdff598fc8c11b3bbfcf3194a21dfc30
saram-x <saramkhan9077@gmail.com>
2025-06-02 14:23:29 +0000

    Add AddCompanyDrawer component for company creation UI

---
+++ src/components/add-company-drawer.jsx
@@ -0,0 +1,21 @@
+import {
+    Drawer,
+    DrawerClose,
+    DrawerContent,
+    DrawerFooter,
+    DrawerHeader,
+    DrawerTitle,
+    DrawerTrigger,
+  } from "@/components/ui/drawer";
+  import { Button } from "./ui/button";
+  import { Input } from "./ui/input";
+  import { z } from "zod";
+  import { zodResolver } from "@hookform/resolvers/zod";
+  import { useForm } from "react-hook-form";
+  import useFetch from "@/hooks/use-fetch";
+  import { addNewCompany } from "@/api/apiCompanies";
+  import { BarLoader } from "react-spinners";
+  import { useEffect } from "react";
+  import { useToast } from "@/hooks/use-toast";
+  
+  
