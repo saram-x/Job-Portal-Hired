f77740560401bcb106806265cefc8345ec803ed6
saram-x <saramkhan9077@gmail.com>
2025-06-03 10:43:08 +0000

    Validation schema for company registration form

--- src/components/add-company-drawer.jsx
+++ src/components/add-company-drawer.jsx
@@ -18,4 +18,19 @@ import {
   import { useEffect } from "react";
   import { useToast } from "@/hooks/use-toast";
   
+  // Validation schema for company registration form
+  const schema = z.object({
+    name: z.string().min(1, { message: "Company name is required" }),
+    logo: z
+      .any()
+      .refine(
+        (file) =>
+          file[0] &&
+          (file[0].type === "image/png" || file[0].type === "image/jpeg"),
+        {
+          message: "Only Images are allowed",
+        }
+      ),
+  });
+  
   
