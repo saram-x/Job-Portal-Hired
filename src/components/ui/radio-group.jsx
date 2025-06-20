f05eb2c3e53739d380b2300ae79a6c7559eafec5
saram-x <saramkhan9077@gmail.com>
2025-06-20 16:09:48 +0000

    Add RadioGroup component Custom radio group for job portal forms

---
+++ src/components/ui/radio-group.jsx
@@ -0,0 +1,14 @@
+import * as React from "react"
+import * as RadioGroupPrimitive from "@radix-ui/react-radio-group"
+import { Circle } from "lucide-react"
+
+import { cn } from "@/lib/utils"
+
+const RadioGroup = React.forwardRef(({ className, ...props }, ref) => {
+  return (<RadioGroupPrimitive.Root className={cn("grid gap-2", className)} {...props} ref={ref} />);
+})
+RadioGroup.displayName = RadioGroupPrimitive.Root.displayName
+
+const RadioGroupItem = React.forwardRef(({ className, ...props }, ref) => {
+  return (
+
