7641d51290250eaca4136910da34664afda96b31
saram-x <saramkhan9077@gmail.com>
2025-07-03 21:41:13 +0000

    Refactor: enhanced job application logic and UI in apply-job.jsx

---
+++ src/components/apply-job.jsx
@@ -0,0 +1,22 @@
+import { Button } from "@/components/ui/button";
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
+import { Input } from "./ui/input";
+import { RadioGroup, RadioGroupItem } from "./ui/radio-group";
+import { Label } from "./ui/label";
+import { Controller, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
+import useFetch from "@/hooks/use-fetch";
+import { applyToJob } from "@/api/apiApplication";
+import { BarLoader } from "react-spinners";
+import { useToast } from "@/hooks/use-toast";
+
