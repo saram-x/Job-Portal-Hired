5cc659d96984ba45f403c20f877ff96729d85163
saram-x <saramkhan9077@gmail.com>
2025-07-03 01:21:34 +0000

    Update ApplicationCard: improved status management and UI structure for job applications

--- src/components/application-card.jsx
+++ src/components/application-card.jsx
@@ -51,4 +51,48 @@ const ApplicationCard = ({ application, isCandidate = false }) => {
             onClick={handleDownload}
           />
         </CardTitle>
-  
+      </CardHeader>
+      <CardContent className="flex flex-col gap-4 flex-1">
+        <div className="flex flex-col md:flex-row justify-between">
+          <div className="flex gap-2 items-center">
+            <BriefcaseBusiness size={15} /> {application?.experience} years of
+            experience
+          </div>
+          <div className="flex gap-2 items-center">
+            <School size={15} />
+            {application?.education}
+          </div>
+          <div className="flex gap-2 items-center">
+            <Boxes size={15} /> Skills: {application?.skills}
+          </div>
+        </div>
+        <hr />
+      </CardContent>
+      <CardFooter className="flex justify-between">
+        <span>{new Date(application?.created_at).toLocaleString()}</span>
+        {isCandidate ? (
+          <span className="capitalize font-bold">
+            Status: {application.status}
+          </span>
+        ) : (
+          <Select
+            onValueChange={handleStatusChange}
+            defaultValue={application.status}
+          >
+            <SelectTrigger className="w-52">
+              <SelectValue placeholder="Application Status" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="applied">Applied</SelectItem>
+              <SelectItem value="interviewing">Interviewing</SelectItem>
+              <SelectItem value="hired">Hired</SelectItem>
+              <SelectItem value="rejected">Rejected</SelectItem>
+            </SelectContent>
+          </Select>
+        )}
+      </CardFooter>
+    </Card>
+  );
+};
+
+export default ApplicationCard;
