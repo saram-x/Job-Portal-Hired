50f230e61c912ba86842ac08dcc7985f6aadfc7d
saram-x <saramkhan9077@gmail.com>
2025-07-08 21:13:08 +0000

    Update created-applications.jsx improve API error handling

--- src/components/created-applications.jsx
+++ src/components/created-applications.jsx
@@ -5,3 +5,18 @@ import { getApplications } from "@/api/apiApplication";
 import useFetch from "@/hooks/use-fetch";
 import { BarLoader } from "react-spinners";
 
+// Displays job applications submitted by the current candidate
+const CreatedApplications = () => {
+  const { user } = useUser();
+
+  const {
+    loading: loadingApplications,
+    data: applications,
+    fn: fnApplications,
+  } = useFetch(getApplications, {
+    user_id: user.id,
+  });
+
+  useEffect(() => {
+    fnApplications();
+   
