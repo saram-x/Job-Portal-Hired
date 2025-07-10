87d59ae9223b318d740453559ebad4b52a9cde84
saram-x <saramkhan9077@gmail.com>
2025-07-10 11:26:26 +0000

    Update CreatedJobs: enhance job listing and UI structure

---
+++ src/components/created-jobs.jsx
@@ -0,0 +1,7 @@
+import { getMyJobs } from "@/api/apiJobs";
+import useFetch from "@/hooks/use-fetch";
+import { useUser } from "@clerk/clerk-react";
+import { BarLoader } from "react-spinners";
+import JobCard from "./job-card";
+import { useEffect } from "react";
+
