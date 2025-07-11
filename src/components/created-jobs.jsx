359dea7b4a15dc69b5aa49b550bd9410240ea0f1
saram-x <saramkhan9077@gmail.com>
2025-07-11 07:02:05 +0000

    improve job display and UI layout

--- src/components/created-jobs.jsx
+++ src/components/created-jobs.jsx
@@ -5,3 +5,18 @@ import { BarLoader } from "react-spinners";
 import JobCard from "./job-card";
 import { useEffect } from "react";
 
+// Displays jobs posted by the current recruiter with management controls
+const CreatedJobs = () => {
+  const { user } = useUser();
+
+  const {
+    loading: loadingCreatedJobs,
+    data: createdJobs,
+    fn: fnCreatedJobs,
+  } = useFetch(getMyJobs, {
+    recruiter_id: user.id,
+  });
+
+  useEffect(() => {
+    fnCreatedJobs();
+    
