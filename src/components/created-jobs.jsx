518fe09f5fd720c1ac1475b8863dcf48541b0125
saram-x <saramkhan9077@gmail.com>
2025-07-12 04:46:44 +0000

    Complete Update: improve recruiter job display and loading UI in CreatedJobs component

--- src/components/created-jobs.jsx
+++ src/components/created-jobs.jsx
@@ -19,4 +19,33 @@ const CreatedJobs = () => {
 
   useEffect(() => {
     fnCreatedJobs();
-    
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <div>
+      {loadingCreatedJobs ? (
+        <BarLoader className="mt-4" width={"100%"} color="#36d7b7" />
+      ) : (
+        <div className="mt-8 grid md:grid-cols-2 lg:grid-cols-3 gap-4">
+          {createdJobs?.length ? (
+            createdJobs.map((job) => {
+              return (
+                <JobCard
+                  key={job.id}
+                  job={job}
+                  onJobAction={fnCreatedJobs}
+                  isMyJob
+                />
+              );
+            })
+          ) : (
+            <div>No Jobs Found 😢</div>
+          )}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default CreatedJobs;
