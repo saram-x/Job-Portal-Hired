12ae983521cf250946795eecba7b0e7f534aaf6b
saram-x <saramkhan9077@gmail.com>
2025-07-12 21:49:23 +0000

    Added: update header component for improved navigation and branding

---
+++ src/components/header.jsx
@@ -0,0 +1,12 @@
+import { useEffect, useState } from "react";
+import { Link, useSearchParams } from "react-router-dom";
+import {
+  SignedIn,
+  SignedOut,
+  UserButton,
+  SignIn,
+  useUser,
+} from "@clerk/clerk-react";
+import { Button } from "./ui/button";
+import { BriefcaseBusiness, Heart, PenBox } from "lucide-react";
+
