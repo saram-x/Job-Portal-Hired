a5ec6c5ac309eefa18aa7766deace98243a34809
saram-x <saramkhan9077@gmail.com>
2025-07-15 09:26:20 +0000

    header navigation and user menu for improved UX

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -64,4 +64,37 @@ const Header = () => {
                 },
               }}
             >
-     
+              <UserButton.MenuItems>
+                <UserButton.Link
+                  label="My Jobs"
+                  labelIcon={<BriefcaseBusiness size={15} />}
+                  href="/my-jobs"
+                />
+                <UserButton.Link
+                  label="Saved Jobs"
+                  labelIcon={<Heart size={15} />}
+                  href="/saved-jobs"
+                />
+                <UserButton.Action label="manageAccount" />
+              </UserButton.MenuItems>
+            </UserButton>
+          </SignedIn>
+        </div>
+      </nav>
+
+      {showSignIn && (
+        <div
+          className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50"
+          onClick={handleOverlayClick}
+        >
+          <SignIn
+            signUpForceRedirectUrl="/onboarding"
+            fallbackRedirectUrl="/onboarding"
+          />
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Header;
