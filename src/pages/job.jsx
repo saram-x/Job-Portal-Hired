92790581b5105f6ba8476020aa2d3743b8f2c52f
babar-hassan-x <noorumkhan9@gmail.com>
2025-08-30 23:01:25 +0500

    setup job.jsx with hooks, Clerk user, icons, and markdown editor

---
+++ src/pages/job.jsx
@@ -0,0 +1,7 @@
+import { useEffect } from "react";
+import { BarLoader } from "react-spinners";
+import MDEditor from "@uiw/react-md-editor";
+import { useParams } from "react-router-dom";
+import { useUser } from "@clerk/clerk-react";
+import { Briefcase, DoorClosed, DoorOpen, MapPinIcon } from "lucide-react";
+
