5131aab7cf7ccfe9fce43dd40368c4a3420400b0
rabees-ali-shah <facebookpayut@gmail.com>
2025-08-30 22:55:53 +0500

    configure Clerk publishable key from environment variables in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,3 +5,6 @@ import "./index.css";
 import { ClerkProvider } from "@clerk/clerk-react";
 import { shadesOfPurple } from "@clerk/themes";
 
+// Get Clerk authentication key from environment variables
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
