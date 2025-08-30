4ef972b5fecdd2674b58070dabcecf03ef574340
babar-hassan-x <noorumkhan9@gmail.com>
2025-08-30 23:10:24 +0500

    Job title and company logo header style

--- src/pages/job.jsx
+++ src/pages/job.jsx
@@ -58,4 +58,12 @@ const JobPage = () => {
 
   return (
     <div className="flex flex-col gap-8 mt-5">
+      {/* Job title and company logo header */}
+      <div className="flex flex-col-reverse gap-6 md:flex-row justify-between items-center">
+        <h1 className="gradient-title font-extrabold pb-3 text-4xl sm:text-6xl">
+          {job?.title}
+        </h1>
+        <img src={job?.company?.logo_url} className="h-12" alt={job?.title} />
+      </div>
+
       
