be1ba784ae87491e620b52a5132fadca35be9922
babar-hassan-x <noorumkhan9@gmail.com>
2025-08-30 23:02:38 +0500

    integrate UI components, hooks, and job API functions into job.jsx

--- src/pages/job.jsx
+++ src/pages/job.jsx
@@ -5,3 +5,16 @@ import { useParams } from "react-router-dom";
 import { useUser } from "@clerk/clerk-react";
 import { Briefcase, DoorClosed, DoorOpen, MapPinIcon } from "lucide-react";
 
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { ApplyJobDrawer } from "@/components/apply-job";
+import ApplicationCard from "@/components/application-card";
+
+import useFetch from "@/hooks/use-fetch";
+import { getSingleJob, updateHiringStatus } from "@/api/apiJobs";
+
